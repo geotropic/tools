36bacf0793608904129e927a88d4af0d9a9aeea1
geotropic <100081013+geotropic@users.noreply.github.com>
2022-03-26 14:16:13 -0600

    Add files via upload

---
+++ whois-denied.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+##
+### whois-denied.sh - Search WHOIS records(over tor) for IP's from /etc/hosts.deny
+##	NOTE: Uses tor for whois(whois servers blacklist some tor exit-nodes) and dns resolution
+#
+if [ -z $(which tor) ]; then
+	echo "[!] Error: tor not installed. Install and try again."
+	exit 1
+elif [ $(whoami) != "root" ]; then
+	echo "[!] Error: login as root and try again."
+	exit 1
+fi
+
+cancel() {
+	exit 1
+}
+
+trap cancel SIGINT
+systemctl status tor | grep 'active' 2>&1 > /dev/null
+
+if [ $? == "1" ]; then
+	echo "[-] Starting tor service.."
+	systemctl start tor
+fi
+
+for ip in $(grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' /etc/hosts.deny | sort -R); do
+	echo -e "[-] Looking up WHOIS Record for \e[0;94m$ip\e[0m (\e[0;92m\e[4m$(tor-resolve -x $ip 2>/dev/null)\e[0m).."
+	torify whois $ip | grep -E 'inet|escr|wner|ame|ange|ountry|tate|ovince|ity' 
+	
+	if [ $? == "1" ]; then
+		echo -e "[!] \e[0;91mNo record found for IP:\e[0m \e[0;94m$ip\e[0m (\e[0;92m\e[4m$(tor-resolve -x $ip 2>/dev/null)\e[0m)."
+	fi
+	
+	echo ""
+	sleep 2
+done
+
