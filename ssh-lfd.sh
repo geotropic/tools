e3ef04dbac278ca30bad8ccc16f8125159948070
geotropic <100081013+geotropic@users.noreply.github.com>
2022-03-29 19:58:12 -0600

    improvements
    fixed bash script warnings, used an array instead of a file to parse hosts

--- ssh-lfd.sh
+++ ssh-lfd.sh
@@ -10,41 +10,42 @@ export TMPFILE="/tmp/ssh-lfd.tmp"
 export HOSTLIST="/tmp/ssh-lfd.hosts"
 
 banner() {
-  log ""
-  log "  ssh-lfd.sh - l(ogin)-f(ail)-d(eny)"
-  log ""
+	log ""
+	log "  ssh-lfd.sh - l(ogin)-f(ail)-d(eny)"
+	log ""
 }
 
 log() {
-  echo $1
-  echo $1 >> $LOGFILE
+	echo $1
+	echo $1 >> $LOGFILE
 }
 
 cancel() {
-  rm -f $TMPFILE $HOSTLIST
+	rm -f $TMPFILE $HOSTLIST
 }
 
 trap cancel SIGINT
 
 if [ $(whoami) != "root" ]; then 
-  banner
-  echo "[!] Error: login as root and try again."
-  exit
+	banner
+	echo "[!] Error: login as root and try again."
+	exit
 fi
 
 touch $TMPFILE
 cat $AUTHLOG | grep sshd | grep "checking getaddrinfo" | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' >> $TMPFILE
 cat $AUTHLOG | grep sshd | grep "Failed password for root" | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' >> $TMPFILE
-sort -u $TMPFILE > $HOSTLIST
+hosts=($(sort -u $TMPFILE))
 rm -f $TMPFILE
 
-for item in $(cat $HOSTLIST); do
-  if [ -z "$(grep $item $DENYHOST)" ]; then
-    log "[+] $(date): adding $item to $DENYHOST"
-    echo "sshd:$item" >> $DENYHOST 
-  else
-    continue
-  fi
-done
+for item in ${hosts[@]}; do
+	grep $item $DENYHOST 2>&1 > /dev/null
 
-rm -f $HOSTLIST 
+	if [ $? == "1" ]; then
+		log "[+] $(date): adding $item to $DENYHOST"
+		echo "sshd:$item" >> $DENYHOST
+	elif [ $? == "0" ]; then
+		continue
+	fi
+
+done
