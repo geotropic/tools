a51a47d325fd16a188b3b8485d415dd627ad513d
geotropic <100081013+geotropic@users.noreply.github.com>
2022-04-06 13:04:22 -0600

    fixes
    Error check for starting tor was inverted and is now correct. Added delay after starting tor to allow the circuit to build.

--- whois-denied.sh
+++ whois-denied.sh
@@ -18,14 +18,15 @@ cancel() {
 trap cancel SIGINT
 systemctl status tor | grep 'active' 2>&1 > /dev/null
 
-if [ $? == "1" ]; then
+if [ $? == "0" ]; then
 	echo "[-] Starting tor service.."
+	sleep 3	
 	systemctl start tor
 fi
 
 for ip in $(grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' /etc/hosts.deny | sort -uR); do
 	echo -e "[-] Looking up WHOIS Record for \e[0;94m$ip\e[0m (\e[0;92m\e[4m$(tor-resolve -x $ip 2>/dev/null)\e[0m).."
-	torify whois $ip | grep -E 'inet|escr|wner|ame|ange|ountry|tate|ovince|ity' 
+	torify whois $ip | grep -E 'enied|inet|escr|wner|ame|ange|ountry|tate|ovince|ity' 
 	
 	if [ $? == "1" ]; then
 		echo -e "[!] \e[0;91mNo record found for IP:\e[0m \e[0;94m$ip\e[0m (\e[0;92m\e[4m$(tor-resolve -x $ip 2>/dev/null)\e[0m)."
